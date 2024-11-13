ec25e5cf02df1e57045692fd919dd687a59e0883
jbw9964 <jbw9964@gmail.com>
2024-11-13 12:14:24 +0900

    ci4

---
+++ App2/src/main/java/core/app2/App2Application.java
@@ -0,0 +1,13 @@
+package core.app2;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class App2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App2Application.class, args);
+    }
+
+}
